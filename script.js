794370e58067442e346262fd3f9974783366bfbd
Giulia <giuliadbem@gmail.com>
2024-01-18 22:26:42 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if(html.classList.contains("light")) {
+    // se tiver light mode, adicionar imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+
+  } else {
+    // se tiver sem light mode, manter imagem original
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+  
+  
+  
+}
